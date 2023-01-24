2026b88aa6d69b611ad81c5e95c842a2428ce7a7
Chadlamardo <114991049+on-rehab@users.noreply.github.com>
2023-01-24 12:11:00 +0100

    Added WiFi capabilities.

--- main/main.c
+++ main/main.c
@@ -115,6 +115,48 @@ static void mqtt_app_start(esp_mqtt_client_handle_t client)
     esp_mqtt_client_start(client);
 }
 
+static void wifi_event_handler(void * event_handler_arg, esp_event_base_t event_base, int32_t event_id, void * event_data) {
+  switch (event_id) {
+  case WIFI_EVENT_STA_START:
+    ESP_LOGI(TAG, "WiFi connecting ... \n");
+    break;
+  case WIFI_EVENT_STA_CONNECTED:
+    ESP_LOGI(TAG, "WiFi connected ... \n");
+    break;
+  case WIFI_EVENT_STA_DISCONNECTED:
+    ESP_LOGI(TAG, "WiFi lost connection ... \n");
+    break;
+  case IP_EVENT_STA_GOT_IP:
+    ESP_LOGI(TAG, "WiFi got IP ... \n\n");
+    break;
+  default:
+    break;
+  }
+}
+
+void wifi_connection() {
+  // 1 - Wi-Fi/LwIP Init Phase
+  esp_netif_init(); // TCP/IP initiation      s1.1
+  esp_event_loop_create_default(); // event loop                    s1.2
+  esp_netif_create_default_wifi_sta(); // WiFi station                      s1.3
+  wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();
+  esp_wifi_init( & wifi_initiation); //                          s1.4
+  // 2 - Wi-Fi Configuration Phase
+  esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);
+  esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
+  wifi_config_t wifi_configuration = {
+    .sta = {
+      .ssid = SSID,
+      .password = PASS
+    }
+  };
+  esp_wifi_set_config(ESP_IF_WIFI_STA, & wifi_configuration);
+  // 3 - Wi-Fi Start Phase
+  esp_wifi_start();
+  // 4- Wi-Fi Connect Phase
+  esp_wifi_connect();
+}
+
 void app_main(void)
 {
     ESP_LOGI(TAG, "[APP] Startup..");
@@ -137,6 +179,9 @@ void app_main(void)
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();
+    wifi_connection();
 
     esp_mqtt_client_config_t mqtt_cfg = {
         .uri = "mqtt://demo.thingsboard.io",
